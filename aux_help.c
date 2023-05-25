a683a55aff986bef77e87fd132b2dadf8dec11ab
chiomavictoriannadi <chiomavictoriannadi@gmail.com>
2023-05-24 21:24:29 -0700

    file that helps on information for the builtin environment

--- aux_help.c
+++ aux_help.c
@@ -1,8 +1,8 @@
 #include "shell.h"
 
 /**
- * aux_help_env - Help information for the builtin env
- * Return: no return
+ * aux_help_env - The builtin env help information for this code
+ * Return: no return may not be allowed
  */
 void aux_help_env(void)
 {
@@ -14,8 +14,8 @@ void aux_help_env(void)
 
 }
 /**
- * aux_help_setenv - Help information for the builtin setenv
- * Return: no return
+ * aux_help_setenv - The builtin setenv help information for this code
+ * Return: no return may not be allowed
  */
 void aux_help_setenv(void)
 {
@@ -29,8 +29,8 @@ void aux_help_setenv(void)
 	write(STDOUT_FILENO, help, _strlen(help));
 }
 /**
- * aux_help_unsetenv - Help information for the builtin unsetenv
- * Return: no return
+ * aux_help_unsetenv - The builtin unsetenv help information for this code
+ * Return: no return may not be allowed
  */
 void aux_help_unsetenv(void)
 {
@@ -43,8 +43,8 @@ void aux_help_unsetenv(void)
 
 
 /**
- * aux_help_general - Entry point for help information for the help builtin
- * Return: no return
+ * aux_help_general - This is the entry point for help information for the help builtin
+ * Return: no return may not be allowed
  */
 void aux_help_general(void)
 {
@@ -65,8 +65,8 @@ void aux_help_general(void)
 	write(STDOUT_FILENO, help, _strlen(help));
 }
 /**
- * aux_help_exit - Help information fot the builint exit
- * Return: no return
+ * aux_help_exit - The builtin exit help information for the code
+ * Return: no return may not be allowed
  */
 void aux_help_exit(void)
 {
