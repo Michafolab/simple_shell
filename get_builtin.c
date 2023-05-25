a1662a63fa21bb2d93c61c57962ebb9e20e0478a
chiomavictoriannadi <chiomavictoriannadi@gmail.com>
2023-05-25 11:26:43 -0700

    function pointer of the builtin command

--- get_builtin.c
+++ get_builtin.c
@@ -1,9 +1,9 @@
 #include "shell.h"
 
 /**
- * get_builtin - builtin that pais the command in the arg
+ * get_builtin - The command is paised by the builtin in the arg
  * @cmd: command
- * Return: function pointer of the builtin command
+ * Return: The builtin command function pointer
  */
 int (*get_builtin(char *cmd))(data_shell *)
 {
